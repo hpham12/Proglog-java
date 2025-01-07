09467aa1fcf6f05bf69f417ca846cbba79e1357d
Hieu Pham <hieuminhphamcse21@gmail.com>
2025-01-07 11:47:14 +0700

    fix: rename package

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "org.proglog"
 version = "1.0-SNAPSHOT"
 
 repositories {
---
+++ src/main/java/org/proglog/Main.java
@@ -0,0 +1,7 @@
+package org.proglog;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Distributed commit log");
+    }
+}
